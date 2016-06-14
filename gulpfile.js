dd9c2584eafb5505aaa5cb60b50107a62a13082c
Marta Fonda <martafondapascual@gmail.com>
2016-06-14 21:53:23 +0200

    Gulp configuration for server and css compilation and build

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,36 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var webserver = require('gulp-webserver');
+var runSequence = require('run-sequence');
 
+gulp.task('default', function(callback){
+  runSequence('build', callback);
+})
 
-gulp.task('sass', function () {
+gulp.task('serve', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+      port: 9090,
+      directoryListing: {
+        enable:true,
+        path: 'app'
+      },
+      open: true
+    }));
+});
+
+//Compiling and Building SASS Files
+gulp.task('build-css', function () {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./scss'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('build-css:watch', function () {
   gulp.watch('./scss/**/*.scss', ['sass']);
 });
+
+
+gulp.task('build', ['build-css', 'serve']);
