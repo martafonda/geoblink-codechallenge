36d9fbd5ab77931d0feecd32740bd203f587840e
Marta Fonda <martafondapascual@gmail.com>
2016-06-15 14:15:18 +0200

    Testing method color for controller chartCtrl

--- tests/spec-chart.js
+++ tests/spec-chart.js
@@ -8,12 +8,33 @@ describe('Chart', function(){
 
     describe('$scope.color', function(){
 
-      it('returns true', function() {
+      it('sets color #5Ab1BB to the area 0 of the chart', function() {
+        var $scope = {};
+        var controller = $controller('chartCtrl', { $scope: $scope });
 
-        expect(true).toEqual(true);
+        index = 0;
+        expect($scope.color(index)).toEqual('#5Ab1BB');
+      });
+
+      it('sets color #F7DD72 to the area 1 of the chart', function() {
+        var $scope = {};
+        var controller = $controller('chartCtrl', { $scope: $scope });
+
+        index = 1;
+        expect($scope.color(index)).toEqual('#F7DD72');
+      });
+
+      it('sets color #225E65 to the area 2 of the chart', function() {
+        var $scope = {};
+        var controller = $controller('chartCtrl', { $scope: $scope });
+
+        index = 2;
+        expect($scope.color(index)).toEqual('#225E65');
       });
 
     });
+
+
 
   });
 
